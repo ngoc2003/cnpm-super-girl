bdc97d4a39a5e6d3b819ee368a2d1ea710153ec0
Bui Ngoc <buingoc@Ahihos-MacBook-Pro.local>
2022-12-14 19:41:08 +0700

    change URL auth

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // export const apiURL = "https://crowfundingdb.vercel.app/api";
-export const apiURL = "http://localhost:3001/api";
+export const apiURL = "https://supergirl-db.vercel.app/api";
 export const imgbbAPI = `https://api.imgbb.com/1/upload?key=fbccfab96dc9f2c5249a9f2fe1ed677f`;
 export const TitleDocument = "Super Girl";
