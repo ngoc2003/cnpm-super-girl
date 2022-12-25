fc62bfd9dc2809090321a7b4c9a72b98c51d89c5
Bui Ngoc <buingoc@Ahihos-MacBook-Pro.local>
2022-12-25 14:39:14 +0700

    update  thumbnail

--- src/pages/HomePageUser.js
+++ src/pages/HomePageUser.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from "react";
 import Hero from "./homepage/Hero";
 import QuickLinks from "./homepage/user/QuickLinks";
 import SpecialCollections from "./homepage/user/SpecialCollections";
 import Upcoming from "./homepage/user/Upcoming";
 const HomePageUser = () => {
-  // const navigate = useNavigate();
-  // const { user } = useSelector((state) => state.auth);
-
   return (
     <>
       <Hero />
